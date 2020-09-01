66a50c5d38d1108010a7e582465c4b95ad4d2ab4
Seifo <seifallah27@gmail.com>
2020-09-01 16:01:42 +0200

    Ajouter formulaire + template Inscription

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,11 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\InscriptionFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
+
+
+    private $manger;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->manager = $entityManager;
+    }
+
+
     /**
      * @Route("/user", name="user")
      */
@@ -16,4 +31,42 @@ class UserController extends AbstractController
             'controller_name' => 'UserController',
         ]);
     }
+
+
+    /**
+     * @Route("/inscrit", name="inscription")
+     */
+    public function inscription(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+
+        $form = $this->createForm(InscriptionFormType::class);   // on  enlevé $user et on l'appel dans le test if avec $user = $form->getData(); 
+         $form->handleRequest($request);
+
+
+         if ($form->isSubmitted() && $form->isValid()) {
+
+            $user = $form->getData();
+            $user->setDateEnregistrement(new \DateTime('now'));
+            $password = $form->get('plainPassword')->getData();
+
+            //hash du mot de passe et création du jeton 
+            $user->setPassword($passwordEncoder->encodePassword($user, $password));
+
+            //$entityManager = $this->getDoctrine()->getManager(); => cette ligne est remplacer l' appel a EntityManagerInterface dans la methode
+            $this->manager->persist($user);
+            $this->manager->flush();
+     
+
+
+            $this->addFlash('success', 'Vous avez bien été inscrit ! ');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('user/inscription.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    
 }
