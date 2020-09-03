aa3870a4cd9ceda185e28e7ffcb1d16d1a9144eb
Seifo <seifallah27@gmail.com>
2020-09-03 16:38:03 +0200

    ajouter route une fois qu'on click sur participation par utilisateur ou non

--- src/Controller/ParticipeController.php
+++ src/Controller/ParticipeController.php
@@ -16,6 +16,7 @@ class ParticipeController extends AbstractController
     public function index(Event $event)
     {
         dd($event);
+
             $this->addFlash('success', 'Vous participer à l\'événemnt');
             return $this->redirectToRoute('event_page', ['id' => $event->getId()]);
     }
